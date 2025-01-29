55c5fe135a7e70b8b3a0889a65dc938fa5ca6f29
prlgadminuser <liam.heizmann@gmail.com>
2025-01-29 18:46:34 +0100

    Add files via upload

--- routes/getShopData.js
+++ routes/getShopData.js
@@ -7,24 +7,24 @@ async function getshopdata() {
   currentDate.setHours(0, 0, 0, 0);
   const t0am = currentDate.getTime();
 
-try {
-  // Find the daily items in the shop
-   const dailyItems = "dailyItems";
-  const itemshop = await shopcollection.findOne({ _id: dailyItems });
+  try {
+    // Find the daily items in the shop
+    const dailyItems = "dailyItems";
+    const itemshop = await shopcollection.findOne({ _id: dailyItems });
 
-   if (!itemshop) {
+    if (!itemshop) {
+      throw new Error("error");
+    }
+
+    return ({
+      dailyItems: itemshop.items, // Or items directly if you don't need an array
+      shoptheme: itemshop.theme,
+      server_nexttime: t0am
+    });
+  } catch (error) {
+    // console.error("Error fetching daily items:", error);
     throw new Error("error");
   }
-
-  return({
-    dailyItems: itemshop.items, // Or items directly if you don't need an array
-    shoptheme: itemshop.theme,
-    server_nexttime: t0am
-  });
-} catch (error) {
- // console.error("Error fetching daily items:", error);
- throw new Error("error");
-}
 }
 
 
