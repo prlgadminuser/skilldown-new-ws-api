f71da7e2122f4ae5faee3b4d1ddaf8a37eb83df4
prlgadminuser <liam.heizmann@gmail.com>
2025-01-30 23:12:38 +0100

    Update limitconfig.js

--- limitconfig.js
+++ limitconfig.js
@@ -18,8 +18,8 @@ function createRateLimiter() { // connected sending message rate limit
 }
 
 const getClientIp = (req) => {
-  const clientip = req.connection.remoteAddress
-  //console.log(clientip
+  const clientip = req.headers['x-forwarded-for']?.split(',')[0]
+  console.log(clientip)
   return clientip
 };
 // back4app ip = req.headers['x-forwarded-for']?.split(',')[0]
