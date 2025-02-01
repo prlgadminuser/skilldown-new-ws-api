3cfdda8b7b6a62c3c5e83f46702581451490f785
prlgadminuser <liam.heizmann@gmail.com>
2025-02-01 23:15:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -304,7 +304,7 @@ wss.on("connection", (ws, req) => {
     ws.send(JSON.stringify({ type: "connection_success", accdata: playerVerified.inventory }));
 
     const pingIntervalId = setInterval(() => {
-        if (ws && playerVerified.lastPongTime > Date.now() - pingInterval) {
+        if (ws && playerVerified.lastPongTime > Date.now()) {
         } else {
             ws.close(3845, "activity timeout");
         }
